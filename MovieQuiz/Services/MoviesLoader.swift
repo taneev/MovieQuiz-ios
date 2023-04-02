3e19bfd87768791dc61204ddc8ab33d9d3f5211d
Timur Taneev <taneev@yandex.ru>
2023-04-02 21:31:24 +0200

    sprint 6: работа с сетью

---
+++ MovieQuiz/Services/MoviesLoader.swift
@@ -0,0 +1,47 @@
+//
+//  MoviesLoader.swift
+//  MovieQuiz
+//
+//  Created by Тимур Танеев on 30.03.2023.
+//
+
+import Foundation
+
+protocol MoviesLoading {
+    func loadMovies(handler: @escaping (Result<MostPopularMovies, Error>) -> Void)
+}
+
+struct MoviesLoader: MoviesLoading {
+    private enum LoaderError: Error {
+        case loaderDecodeError
+    }
+
+    // MARK: - NetworkClient
+    private let networkClient = NetworkClient()
+
+    // MARK: - URL
+    private var mostPopularMoviesUrl: URL {
+        guard let url = URL(string: "https://imdb-api.com/en/API/MostPopularMovies/k_2m3tg8et") else {
+            preconditionFailure("Unable to construct mostPopularMoviesUrl")
+        }
+        return url
+    }
+
+    func loadMovies(handler: @escaping (Result<MostPopularMovies, Error>) -> Void) {
+        networkClient.fetch(url: mostPopularMoviesUrl) {result in
+            switch result {
+            case .failure(let error):
+                handler(.failure(error))
+            case .success(let data):
+                do {
+                    let mostPopularMovies = try JSONDecoder().decode(MostPopularMovies.self, from: data)
+                    handler(.success(mostPopularMovies))
+                    return
+                }
+                catch {
+                    handler(.failure(error))
+                }
+            }
+        }
+    }
+}
